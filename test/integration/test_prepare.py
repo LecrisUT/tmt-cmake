f0fc3fba16eb62140126283d5f5550f1cef331e2
Cristian Le <git@lecris.dev>
2024-05-11 08:41:52 +0200

    [Debug] Try pytest-subprocess
    Signed-off-by: Cristian Le <git@lecris.dev>

--- test/integration/test_prepare.py
+++ test/integration/test_prepare.py
@@ -6,38 +6,16 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pytest_check.context_manager import CheckContextManager
+    from pytest_subprocess.fake_process import FakeProcess
 
 DATA_DIR = (Path(__file__).parent / "data" / "prepare").resolve()
 
 
 def test_default(
+    fp: FakeProcess,
     check: CheckContextManager,
     tmt_cli,
 ) -> None:
-    """
-    TODO: Mock the cli calls. Blocked by:
-    https://github.com/flexmock/flexmock/pull/162
-
-    .. code-block:: python
-
-        configure_cmd = flexmock(Command)
-        configure_cmd.should_receive("run").once()
-        build_cmd = flexmock(Command)
-        build_cmd.should_receive("run").once()
-        mock_command = flexmock(Command)
-        (
-            mock_command.should_receive("__new__")
-            .with_args("cmake", str, str)
-            .and_return(configure_cmd)
-            .once()
-        )
-        (
-            mock_command.should_receive("__new__")
-            .with_args("cmake", str, str)
-            .and_return(build_cmd)
-            .once()
-        )
-    """
     args = [
         f"--root={DATA_DIR}",
         "run",
@@ -50,6 +28,9 @@ def test_default(
         "plan",
         "--name=default",
     ]
+    fp.register(["cmake", fp.any(min=2)], returncode=1)
+    fp.register(["cmake", "--build", fp.any(min=1)], returncode=1)
+    fp.allow_unregistered(allow=True)
     result = tmt_cli(args)
     check.equal(result.exit_code, 0)
     check.is_true(re.search(r"cmd: cmake -S.* -B.*", result.stdout))
